4259faa85acedee784466571bfe622efae5abbb7
Saurabh Pawale <saurabh.pawale@mrisoftware.com>
2022-07-25 15:43:48 +0530

    test for correct url

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as path from 'path';
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from "path";
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myFunction = new NodejsFunction(this, 'createMsTeamNotification', {
+    const myFunction = new NodejsFunction(this, "createMsTeamNotification", {
       memorySize: 1024,
       timeout: cdk.Duration.seconds(5),
       runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'main',
-      entry: path.join(__dirname, '/../src/my-lambda/index.ts'),
+      handler: "createMsTeamNotification",
+      entry: path.join(__dirname, "functions/create-ms-team-notification.ts"),
     });
   }
 }
