a385b140ec3438529a62ae88aa4a7da108b1c7ff
Harsh Rohila <harsh.rohila@mrisoftware.com>
2022-07-25 11:56:42 +0530

    using master branch instead of main

--- .projenrc.js
+++ .projenrc.js
@@ -1,11 +1,17 @@
-const { AwsCdkConstructLibrary } = require('projen');
-const project = new AwsCdkConstructLibrary({
-  author: 'Harsh Rohila',
-  authorAddress: 'harsh.rohila@mrisoftware.com',
-  cdkVersion: '1.95.2',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-codepipeline-ms-teams-notifications',
-  repositoryUrl: 'https://github.com/harsh.rohila/cdk-codepipeline-ms-teams-notifications.git',
+const { awscdk } = require("projen");
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: "Harsh Rohila",
+  authorAddress: "harsh.rohila@mrisoftware.com",
+  cdkVersion: "2.0.0",
+  defaultReleaseBranch: "master",
+  name: "cdk-codepipeline-ms-teams-notifications",
+  repositoryUrl:
+    "https://github.com/harsh.rohila/cdk-codepipeline-ms-teams-notifications.git",
+  tsconfig: {
+    compilerOptions: {
+      skipLibCheck: true,
+    },
+  },
 
   // cdkDependencies: undefined,      /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,  /* AWS CDK modules required for testing. */
